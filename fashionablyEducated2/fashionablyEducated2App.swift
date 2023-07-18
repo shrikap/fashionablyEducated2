5f5a2b1756ed25718a209f7dd60922f63ece4882
shrikap <shrikapunuganti@gmail.com>
2023-07-18 11:41:02 -0700

    Initial Commit

---
+++ fashionablyEducated2/fashionablyEducated2App.swift
@@ -0,0 +1,17 @@
+//
+//  fashionablyEducated2App.swift
+//  fashionablyEducated2
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct fashionablyEducated2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
